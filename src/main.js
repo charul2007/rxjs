804102216c260d835957932f912c23d0220cd18b
Charul <charulchavda1@Charuls-MacBook-Pro.local>
2017-10-01 12:54:20 -0700

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,63 @@
 const $ = require('jquery');
 const Rx = require('rxjs/Rx');
 
-console.log('hello world!');
+let btn = $('#btn');
+let input = $('#input');
+let numbers = [1, 2, 3, 4, 5];
+let posts = [
+  {title: 'Post One', body: 'I am the body'},
+  {title: 'Post Two', body: 'I am the body'},
+  {title: 'Post Three', body: 'I am the body'}
+];
+
+let btnStream$ = Rx.Observable.fromEvent(btn, 'click');
+let inputStream$ = Rx.Observable.fromEvent(input, 'keyup');
+let numberStream$ = Rx.Observable.from(numbers);
+let postStream$ = Rx.Observable.from(posts);
+
+btnStream$.subscribe(
+  function (e) {
+    console.log('clicked');
+  },
+  function (err) {
+    console.log(err);
+  },
+  function (completed) {
+    console.log('completed');
+  }
+);
+
+inputStream$.subscribe(
+  function (e) {
+    console.log(e.target.value);
+  },
+  function (err) {
+    console.log(err);
+  },
+  function (completed) {
+    console.log('completed');
+  }
+);
+
+numberStream$.subscribe(n => {
+    console.log(n);
+  },
+  err => {
+    console.log(err);
+  },
+  completed => {
+    console.log('completed');
+  }
+)
+
+postStream$.subscribe(
+  posts => {
+    console.log(posts);
+  },
+  err => {
+    console.log(err);
+  },
+  completed => {
+    console.log('completed');
+  }
+)
